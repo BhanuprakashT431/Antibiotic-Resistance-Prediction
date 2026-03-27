89ba3cd14510bbf6858489f9db1368d452ba6723
Bhanuprakasht431 <bhanuprakaahs8@gmail.com>
2026-03-27 23:12:58 +0530

    Apk installation as been used

---
+++ frontend/public/service-worker.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = "app-cache-v1";
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(["/"]);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((res) => res || fetch(event.request))
+    );
+});
